57b80f76aebb007def749e885962b70e55b0eae9
slong96 <94485068+slong96@users.noreply.github.com>
2024-07-11 08:07:15 -0500

    placed code inside function and added try except block

--- file_organizer.py
+++ file_organizer.py
@@ -1,38 +1,50 @@
-# file organizer
-
 import os
 import shutil
 
-# checking if path exists
-while True:
-    path = input("Enter path (/User/John/Example): ")
+def file_organize():
+    # Checking if path exists
+    while True:
+        path = input("Enter path (User/John/Example) or (User\John\Example): ")
 
-    if os.path.exists(path):
-        break
-    else:
-        print("Path not found. Please enter a valid path.")
+        if os.path.exists(path):
+            break
+        else:
+            print("Path not found. Please enter a valid path.")
 
-files = os.listdir(path) # consist of list of files
+    files = os.listdir(path)  # Consist of list of files
+    index = 0
 
-"""
-for loop to travel through every file,
-split the file name and extension of the files.
-"""
-for file in files:
-    filename, extension = os.path.splitext(file)
-    # only need the extension name, removing the dot with slicing.
-    extension = extension[1:]
-    
-    """
-    if the extension folder (mp3, jpeg, zip, etc) exist,
-    move the file to that folder
+    # For loop to travel through every file, split the file name and extension of the files.
+    for file in files:
+        filename, extension = os.path.splitext(file)
+        # Only need the extension name, removing the dot with slicing.
+        extension = extension[1:]
+        
+        # Ignore directories
+        if os.path.isdir(os.path.join(path, file)):
+            continue
 
-    else, make new folder and then move the file into it.
-    """
-    if os.path.exists(f'{path}/{extension}'):
-        shutil.move(f'{path}/{file}', f'{path}/{extension}/{file}')
-    else:
-        os.makedirs(f'{path}/{extension}')
-        shutil.move(f'{path}/{file}', f'{path}/{extension}/{file}')
-    
-print(f'Files are now organized.')
+        try:
+            # If the extension folder (mp3, jpeg, zip, etc) exist, move the file to that folder
+            # Else, make new folder and then move the file into it.
+            if os.path.exists(f'{path}/{extension}'):
+                shutil.move(f'{path}/{file}', f'{path}/{extension}/{file}')
+
+            else:
+                os.makedirs(f'{path}/{extension}')
+                print(f"The '{extension}' folder has been created.")
+
+                shutil.move(f'{path}/{file}', f'{path}/{extension}/{file}')
+
+            index+=1
+            print(f"{index}: '{file}' has been moved to the '{extension}' folder.")
+
+        except PermissionError as e:
+            print(f"Could not move {file}. Reason: {e}")
+        except Exception as e:
+            print(f"An error occurred with {file}. Reason: {e}")
+
+file_organize()
+
+# Prompt to close the program
+input("Enter any key to close the program: ")
